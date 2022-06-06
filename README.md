e20a6baa2c86a09929788249804d8cf1e25d133a
CWIN77 <cwstbp0426@gmail.com>
2022-06-07 00:06:59 +0900

    weditor project for aws hosting

---
+++ README.md
@@ -0,0 +1,4 @@
+### project setting
+# npm install
+### project run hot loading server
+# npm run dev
---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styled from 'styled-components'
+
+import SVG_people from '../svgs/people.svg'
+
+const Container = styled.div`
+  
+`
+
+const Home: NextPage = () => {
+  return (
+    <Container>
+      <div>MyApp</div>
+      <SVG_people width={36} height={36} />
+    </Container>
+  )
+}
+
+export default Home
