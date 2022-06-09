ff9427051ec645bdb8c3d9136dc48dd8f16183ba
CWIN <81596708+CWIN77@users.noreply.github.com>
2022-06-09 19:59:40 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,38 +1,13 @@
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
-
-module.exports = withBundleAnalyzer({
-  target: "serverless",
-  env: {
-    BASE_URL: process.env.BASE_URL,
-  },
-
-  webpack(conf) {
-    conf.module.rules.push({
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.module.rules.push({
       test: /\.svg$/,
-      use: ['@svgr/webpack',
-        {
-          loader: "@svgr/webpack",
-          options: {
-            svgoConfig: {
-              plugins: [
-                {
-                  // Enable figma's wrong mask-type attribute work
-                  removeRasterImages: false,
-                  removeStyleElement: false,
-                  removeUnknownsAndDefaults: false,
-                  // Enable svgr's svg to fill the size
-                  removeViewBox: false,
-                },
-              ],
-            },
-          },
-        },
-      ],
-    });
-    // 절대경로
-    conf.resolve.modules.push(__dirname);
-    return conf;
+      use: ['@svgr/webpack'],
+    })
+    return config
   },
-});
+}
+
+module.exports = nextConfig
